c9721be87554189c2833034be1f10889ce775888
nap111 <likangxxx@aliyun.com>
2019-10-06 17:07:39 +0800

    方法

--- src/main/java/com/op/demo.java
+++ src/main/java/com/op/demo.java
@@ -4,5 +4,10 @@ public class demo {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("aaaaa");
+
+    }
+
+    public void test1() {
+
     }
 }
